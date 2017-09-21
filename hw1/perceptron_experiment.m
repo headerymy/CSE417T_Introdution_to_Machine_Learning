5e7894d53ec74a1c181d2218006a3453f08058b0
yang533 <yang533@ACCOUNTS.AD.WUSTL.EDU>
2017-09-21 00:45:24 +0000

    hw

--- hw1/perceptron_experiment.m
+++ hw1/perceptron_experiment.m
@@ -1,13 +1,41 @@
-function [ num_iters bounds] = perceptron_experiment ( N, d, num_samples )
+function [num_iters, bounds] =  perceptron_experiment(N, d, num_samples)
 %perceptron_experiment Code for running the perceptron experiment in HW1
 %   Inputs: N is the number of training examples
 %           d is the dimensionality of each example (before adding the 1)
 %           num_samples is the number of times to repeat the experiment
 %   Outputs: num_iters is the # of iterations PLA takes for each sample
-%            bounds is the theoretical bound on the # of iterations
-%              for each sample
+%            bound_minus_ni is the difference between the theoretical bound
+%               and the actual number of iterations
 %      (both the outputs should be num_samples long)
-
- 
+    
+    for n = 1 : num_samples
+        % set the weight vector
+        weight = [0 rand(1, d)]';
+        % random traning set
+        training = 2 * rand(d, N) - 1;
+        
+        % get the label
+        label = sign(weight(2:end,:)' * training);
+        data = [training; label]';
+        
+        % get weight and iteration
+        [weight_learn, iteration] = perceptron_learn(data);
+        num_iters(n) = iteration;
+        
+        diff = sum(abs(weight_learn - weight(2:end,:)'));
+        
+        max_R = max(norm(training));
+        min2_P = min(label .* ((weight(2:end,:)') * training))^2;
+        bounds(n) = (max_R * norm(weight))/min2_P;
+        diff(n) = bounds(n) - num_iters(n);
+    end
+    
+    figure(1)
+    hist(num_iters)
+    title('Histogram: # of iterations')
+    
+    figure(2)
+    hist(log10(bounds))
+    title('Histogram: difference between # of iterations and bound') 
+    
 end
-
--- hw1/perceptron_learn.m
+++ hw1/perceptron_learn.m
@@ -1,4 +1,4 @@
-function [ w iterations ] = perceptron_learn( data_in )
+function [w, iterations] = perceptron_learn(data_in)
 %perceptron_learn Run PLA on the input data
 %   Inputs: data_in: Assumed to be a matrix with each row representing an
 %                    (x,y) pair, with the x vector augmented with an
@@ -6,6 +6,28 @@ function [ w iterations ] = perceptron_learn( data_in )
 %   Outputs: w: A weight vector (should linearly separate the data if it is
 %               linearly separable)
 %            iterations: The number of iterations the algorithm ran for
-
+   
+    x = data_in( : , 1 : (end - 1));
+    label = data_in( : ,end);
+    num_iters = 0;
+    
+    [num, dim] = size(x);    
+    weight_learn = zeros([dim 1])';
+    condition = 0;
+    
+    while not(condition)
+        condition = 1;
+        for n = 1 : num
+            label_learn = sign(weight_learn * x');
+            if label_learn(n) ~= label(n)
+                weight = weight_learn + x(n, :)*label(n);
+                condition = 0;
+            end
+        end
+        weight_learn = weight;
+        num_iters = num_iters + 1;
+    end
+    w = weight_learn;
+    iterations = num_iters;
 end
 
