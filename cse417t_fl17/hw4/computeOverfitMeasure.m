8b47319588ee8b34b1f7329f1ff300b5794e7d94
yang533@ACCOUNTS.AD.WUSTL.EDU <yang533@ACCOUNTS.AD.WUSTL.EDU@988cfd40-d698-4988-a9d9-e5e9c598e71f>
2017-11-09 03:45:00 +0000

    HW4

--- cse417t_fl17/hw4/computeOverfitMeasure.m
+++ cse417t_fl17/hw4/computeOverfitMeasure.m
@@ -10,3 +10,19 @@ function [ overfit_m ] = computeOverfitMeasure( true_Q_f, N_train, N_test, var, 
 %   Output
 %       overfit_m: vector of length num_expts, reporting each of the
 %                  differences in error between H_10 and H_2
+for i = 1:num_expts
+[train_set, test_set] = generate_dataset( true_Q_f, N_train, N_test, var^0.5);
+z2 = computeLegPoly(train_set(:,1),2);
+z10 = computeLegPoly(train_set(:,1),10);
+
+w2 = glmfit(z2, train_set(:,2), 'normal', 'constant', 'off');
+w10 = glmfit(z10, train_set(:,2), 'normal', 'constant', 'off');
+
+g2 = z2*b2;
+g10 = z10*b10;
+
+E2 = mean((g2(:,1)-test_set(:,2)).^2);
+E10 = mean((g10(:,1)-test_set(:,2)).^2);
+overfit_m(i) = E10 - E2;
+end
+end
--- cse417t_fl17/hw4/generate_dataset.m
+++ cse417t_fl17/hw4/generate_dataset.m
@@ -9,3 +9,17 @@ function [ train_set test_set ] = generate_dataset( Q_f, N_train, N_test, sigma 
 %   Outputs:
 %       train_set and test_set are both 2-column matrices in which each row
 %       represents an (x,y) pair
+train_data = 2*rand(N_train,1)-1;
+test_data = 2*rand(N_test,1)-1;
+train = normrnd(0,1,N_train,1);
+test = normrnd(0,1,N_test,1);
+aq = normrnd(0,1,Q_f+1,1);
+
+c = 0;
+for d = 0:Q_f
+c = c + 1/(2*d+1);
+end
+train_set(:,2) = c^0.5 * computeLegPoly(train_data(:,1), Q_f) * aq + train(:,1) * sigma;
+test_set(:,2) = c^0.5 * computeLegPoly(test_data(:,1), Q_f) * aq + test(:,1) * sigma;
+end
+
