f883c278d746761a05092f13ede5d04475ee7261
yang533 <yang533@ACCOUNTS.AD.WUSTL.EDU>
2017-11-09 03:45:00 +0000

    HW4

--- hw4/computeLegPoly.m
+++ hw4/computeLegPoly.m
@@ -6,3 +6,16 @@ function [ z ] = computeLegPoly( x, Q )
 %   Output:
 %       z: matrix where each column is the Legendre polynomials of order 0 
 %          to Q, evaluated atthe corresponding x value in the input
+z = zeros(size(x));
+if Q==5
+    z = 1/8*(63*x.^5 - 70*x.^3 + 15*x);
+elseif Q==10
+    z = 1/256*(46189*x.^10 - 109395*x.^8 + 90090*x.^6 - 30030*x.^4 + 3465*x.^2 - 63);
+elseif Q==0
+    z = ones(size(x));
+elseif Q==1
+    z = x;
+else
+    z = ((2*Q-1)*x.*computeLegPoly(x,Q-1) - (Q-1)*computeLegPoly(x,Q-2))/(Q);
+end
+end
